e05361411c145b1a93548a87e2f2e96be67d3874
Jason Russell <jasrusable@gmail.com>
2015-04-28 13:38:39 +0200

    set up relationships, add perspective_center

--- assignment_3/points.py
+++ assignment_3/points.py
@@ -7,6 +7,8 @@ class Point(object):
     x = Column(Float, nullable=False)
     y = Column(Float, nullable=False)
     z = Column(Float, nullable=False)
+    marker = Column(String(50))
+    colour = Column(String(50))
 
 class ImagePoint(Base, Point):
     __tablename__ = 'image_point'
@@ -14,12 +16,21 @@ class ImagePoint(Base, Point):
     image_id = Column(Integer, ForeignKey('image.id'))
     image = relationship('Image', back_populates='image_points', cascade='save-update, merge')
 
-    def __init__(self, x, y, z):
-        Point.__init__(self, x=x, y=y, z=z)
+    def __init__(self, x, y, z, colour, marker):
+        Point.__init__(self, x=x, y=y, z=z, colour=colour, marker=marker)
 
 class ObjectPoint(Base, Point):
     __tablename__ = 'object_point'
     id = Column(Integer, primary_key=True)
 
-    def __init__(self, x, y, z):
-        Point.__init__(self, x=x, y=y, z=z)
+    def __init__(self, x, y, z, colour, marker):
+        Point.__init__(self, x=x, y=y, z=z, colour=colour, marker=marker)
+
+class PerspectiveCenterPoint(Base, Point):
+    __tablename__ = 'perspective_center_point'
+    id =Column(Integer, primary_key=True)
+    images = relationship('Image', back_populates='perspective_center', cascade='save-update, merge, delete, delete-orphan')
+
+    def __init__(self, x, y, z, colour, marker, images=list()):
+        self.images = images
+        Point.__init__(self, x=x, y=y, z=z, colour=colour, marker=marker)
