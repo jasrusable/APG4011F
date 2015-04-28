008bccb45e6ef291094a517eb8efe9d689d0d7e0
Jason Russell <jasrusable@gmail.com>
2015-04-28 14:36:50 +0200

    move plot to its own module

--- assignment_3/main.py
+++ assignment_3/main.py
@@ -1,14 +1,11 @@
 import sqlalchemy
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.axes3d import Axes3D
 
 import db
-import matplotlib
-
 from points import Point, ImagePoint, ObjectPoint
 from cameras import Camera
 from images import Image
 from vectors import Vector
+from plot import plot
 
 def init():
 	db.drop_all()
@@ -24,19 +21,6 @@ def init():
 	db.session.commit()
 
 init()
-
-def plot(vectors, points):
-	fig = plt.figure()
-	ax = fig.add_subplot(1, 2, 1, projection='3d')
-	for point in points:
-		assert issubclass(point.__class__, Point)
-		ax.scatter(point.x, point.y, point.z, c='r', marker='o')
-	for vector in vectors:
-		assert issubclass(type(vector), Vector)
-		from_ = vector.from_point
-		to = vector.to_point
-		ax.plot([from_.x, to.x], [from_.y, to.y], zs=[from_.z, to.z])
-	plt.show()
 
 object_points = db.session.query(ObjectPoint).all()
 
---
+++ assignment_3/plot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+
+from points import Point
+from vectors import Vector
+
+def plot(vectors, points):
+	fig = plt.figure()
+	ax = fig.add_subplot(1, 2, 1, projection='3d')
+	for point in points:
+		assert issubclass(point.__class__, Point)
+		ax.scatter(point.x, point.y, point.z, c='r', marker='o')
+	for vector in vectors:
+		assert issubclass(type(vector), Vector)
+		from_ = vector.from_point
+		to = vector.to_point
+		ax.plot([from_.x, to.x], [from_.y, to.y], zs=[from_.z, to.z])
+	plt.show()
+
